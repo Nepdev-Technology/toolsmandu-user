df29d32c353404d7b079ae002cc222a83c2577e1
aayushsapkota9 <aayushsapkota9@gmail.com>
2024-03-21 22:07:59 +0545

    fix: css fixes and product added as component

---
+++ src/components/Cards/Ribbons.tsx
@@ -0,0 +1,13 @@
+const Ribbons = () => {
+  return (
+    <div className="relative overflow-hidden w-56 h-56 bg-white border">
+      <div className="absolute right-0 top-0 h-16 w-16">
+        <div className="absolute transform rotate-45 bg-green-600 text-center text-white font-semibold py-1 right-[-35px] top-[32px] w-[170px]">
+          20% off
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ribbons;
--- src/components/ProductArea/ScrollArea/ScrollAreaClientComponent.tsx
+++ src/components/ProductArea/ScrollArea/ScrollAreaClientComponent.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Box, ScrollArea } from '@mantine/core';
 import { useState } from 'react';
-import ProductCategoryHeader from '../../heading/ProductCategoryHeader';
 
 const ProductScrollAreaClient = ({
   children,
@@ -10,8 +9,9 @@ const ProductScrollAreaClient = ({
 }) => {
   const [categoryTitle, setCategoryTitle] = useState([]);
   return (
-    <div className="mx-10">
-*      <ScrollArea scrollbars="x" type="never">
+    <div>
+      *{' '}
+      <ScrollArea scrollbars="x" type="never">
         <Box className="flex gap-3 overflow-hidden ">{children}</Box>
       </ScrollArea>
     </div>
